4c4ee0bdc46751a3b54d0284bde57fc8181d6206
CompetencesRH <phd7592@gmail.com>
2026-04-19 11:25:38 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,4 +9,5 @@ app.use(express.static('articles'));
 
 const ARTICLES_DIR = path.join(__dirname, 'articles');
 
-async function getArticles({ category, limit = 10 } = {})
+async function getArticles({ category, limit = 10 } = {}) {
+  try
