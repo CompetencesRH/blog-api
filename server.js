8478563bea74194a9933a34681446b75814df029
CompetencesRH <phd7592@gmail.com>
2026-04-19 11:05:41 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const cors = require('cors');
+const fs = require('fs');
+const path = require('path');
 
+const app = express();
+app.use(cors());
+app.use(express.static('articles'));
+
+const ARTICLES_DIR = path.join(__dirname, 'articles');
+
+async function getArticles({ category, limit = 10 } = {})
